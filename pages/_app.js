ea4be70e483961fc6342e32199f6f23606d67718
Sivan Chakravarthy <sivan.sundar@gmail.com>
2023-10-02 11:21:51 +0530

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import AppBridgeProvider from "@/components/providers/AppBridgeProvider";
+import { AppProvider as PolarisProvider } from "@shopify/polaris";
+import "@shopify/polaris/build/esm/styles.css";
+import translations from "@shopify/polaris/locales/en.json";
+import Link from "next/link";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <PolarisProvider
+        i18n={translations}
+        features={{
+          //This is what enables the new monochrome style.
+          //If you want to be on bleeding edge, run `npm install @shopify/polaris@beta`
+          //  instead to install Polaris v12
+          polarisSummerEditions2023: true,
+        }}
+      >
+        <AppBridgeProvider>
+          <ui-nav-menu>
+            <Link href="/debug/data">Fetch Data</Link>
+            <Link href="/debug/billing">Billing API</Link>
+          </ui-nav-menu>
+          <Component {...pageProps} />
+        </AppBridgeProvider>
+      </PolarisProvider>
+    </>
+  );
+}
